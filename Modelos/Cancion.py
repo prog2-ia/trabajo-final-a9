e67fd13bb314847d8d744b578e733c0d61aa108e
Denís <dmm169@gcloud.com>
2026-03-02 13:38:35 +0100

    Class Cancion

--- Modelos/Cancion.py
+++ Modelos/Cancion.py
@@ -10,3 +10,5 @@ class Cancion:
 
     def __str__(self):
         return f'Nombre: {self.nombre} - Duracion: {self.duracion} - Genero: {self.genero} - Artista: {self.artista} - Featuring: {self.featuring} - Idioma: {self.Idioma} - Gentilicio: {self.gentilicio}'
+    def __repr__(self):
+        return f'<{self.nombre} {self.duracion} {self. genero} {self.artista} {self.featuring} {self.idioma} {self.gentilicio}>'
