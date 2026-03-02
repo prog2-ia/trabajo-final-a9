3c3c985f135c93ca6ff271b1014e979038e408f7
Luis <lga85@mscloud.ua.es>
2026-03-02 14:17:51 +0100

    lista generos

--- Modelos/Genero.py
+++ Modelos/Genero.py
@@ -1,3 +1,13 @@
 class Genero:
-    generos = ['Trap', 'Reggaeton', 'Pop', 'Rap', 'Rock', 'Tecnho', 'Clásica', 'Jazz', 'Country']
+    lista_genero = {
+        'Trap': [],
+        'Reggaeton': [],
+        'Pop': [],
+        'Rap': [],
+        'Rock': [],
+        'Tecnho': [],
+        'Clásica': [],
+        'Jazz': [],
+        'Country': []
+    }
     def __init__(self, genero,  ):
--- Modelos/artista.py
+++ Modelos/artista.py
@@ -6,8 +6,19 @@ class Artista:
         self.edad = edad
         self.antiguedad = antiguedad
 
-        def __str__(self):
-            return f'Nombre: {self.nombre} - Duracion: {self.duracion} - Genero: {self.genero} - Artista: {self.artista} - Featuring: {self.featuring} - Idioma: {self.Idioma} - Gentilicio: {self.gentilicio}'
-        def __repr__(self):
-            return f'<{self.nombre} {self.duracion} {self.genero} {self.artista} {self.featuring} {self.idioma} {self.gentilicio}>'
+    def __str__(self):
+        return f'Nombre: {self.nombre} - Duracion: {self.duracion} - Genero: {self.genero} - Artista: {self.artista} - Featuring: {self.featuring} - Idioma: {self.Idioma} - Gentilicio: {self.gentilicio}'
+    def __repr__(self):
+        return f'<{self.nombre} {self.duracion} {self.genero} {self.artista} {self.featuring} {self.idioma} {self.gentilicio}>'
 
+    lista_genero = {
+        'Trap': [],
+        'Reggaeton': [],
+        'Pop': [],
+        'Rap': [],
+        'Rock': [],
+        'Tecnho': [],
+        'Clásica': [],
+        'Jazz': [],
+        'Country': []
+    }
