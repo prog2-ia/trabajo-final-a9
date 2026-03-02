c2efc23a2c6bef89284b577385af7ec797d0a729
Denís <dmm169@gcloud.com>
2026-03-02 13:36:41 +0100

    Comienzo

---
+++ Modelos/Cancion.py
@@ -0,0 +1,12 @@
+class Cancion:
+    def __init__(self, nombre, duracion, genero, artista, featuring, idioma, gentilicio):
+        self.nombre = nombre
+        self.duracion = duracion
+        self.genero = genero
+        self.artista = artista
+        self.featuring = featuring
+        self.idioma = idioma
+        self.gentilicio = gentilicio
+
+    def __str__(self):
+        return f'Nombre: {self.nombre} - Duracion: {self.duracion} - Genero: {self.genero} - Artista: {self.artista} - Featuring: {self.featuring} - Idioma: {self.Idioma} - Gentilicio: {self.gentilicio}'
