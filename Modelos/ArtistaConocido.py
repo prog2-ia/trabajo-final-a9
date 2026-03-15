b566bb6a8ac84c78838d47e3f12610068196102f
Luis <lga85@mscloud.ua.es>
2026-03-15 21:16:36 +0100

    Artista Conocido

--- Modelos/ArtistaConocido.py
+++ Modelos/ArtistaConocido.py
@@ -2,7 +2,7 @@ class ArtistaConocido(Artista):  # Herencia simple de la clase Artista
     def __init__(self, genero, artista, gentilicio, edad, antiguedad, oyentes_mes, premios=0):
         super().__init__(genero, artista, gentilicio, edad, antiguedad, oyentes_mes) #Utilizamos super para recuperar los atributos de Artista
 
-        # Añadimos premios, ya que este atributo no lo heredamos de Artista.
+        # Añadimos premios, ya que este atributo no lo heredamos de Artista
         self.premios = premios
 
     def __str__(self):
